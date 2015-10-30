adfd08dd5b1932bdc4cc5115bd7afc403423ef26
Philip Tibbetts <Phil@dyn-72-33-197-144.uwnet.wisc.edu>
2015-10-30 15:26:34 -0500

    info button

---
+++ ExtViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ExtViewController.swift
+//  MadisonFoodCarts
+//
+//  Created by Philip Tibbetts on 10/19/15.
+//  Copyright © 2015 Philip Tibbetts. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+extension ViewController: MKMapViewDelegate {
+    
+    // 1
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? FoodCart {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                    dequeuedView.annotation = annotation
+                    view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
+}
