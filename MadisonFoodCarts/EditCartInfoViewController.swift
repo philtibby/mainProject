0e010b4b110306124d9cb54632c39287025f83bb
Carly Hildebrandt <ceh630@aim.com>
2015-11-20 15:15:03 -0600

    Added ability to add images from device's photo library to Edit/AddCartViewController classes

--- MadisonFoodCarts/EditCartInfoViewController.swift
+++ MadisonFoodCarts/EditCartInfoViewController.swift
@@ -7,15 +7,64 @@
 //
 
 import UIKit
+// for camera
+import MobileCoreServices
 
-class EditCartInfoViewController: UIViewController {
+class EditCartInfoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
     
     var thisCart: FoodCart?
-
+    
+    
+    // image for cart
+    @IBOutlet weak var cartImage: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    // choosing photo from photo library
+    @IBAction func choosePicture(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    
+    // for choosing image
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            cartImage.contentMode = .ScaleAspectFit
+            cartImage.image = pickedImage
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                message: "Failed to save image",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                completion: nil)
+        }
+    }
+    
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
