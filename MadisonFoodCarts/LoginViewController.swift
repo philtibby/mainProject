8c9f580d435d62e46cb0f09cedc1799ee0ec8592
Carly Hildebrandt <ceh630@aim.com>
2015-12-09 00:03:05 -0600

    I believe the login page is working. You can register for a new account and/or login with that account. It will then take you to the Cart table page where you can see and access only your carts. Operator page is still accessible - this can be changed but more adjustments have to be made so that it isn't needed and new navigation controllers need to be inserted. NOTE: whether you create an operator through the cart operator way or the login way, the operator list will still be updated whenever a new user creates a new cart

--- MadisonFoodCarts/LoginViewController.swift
+++ MadisonFoodCarts/LoginViewController.swift
@@ -7,27 +7,36 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    var matchFound = false
+    
+    @IBOutlet weak var errMsg: UILabel!
     
     @IBOutlet weak var username: UITextField!
     
     @IBOutlet weak var password: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-    // This action allows the register view controller to pop back to here
-    @IBAction func exitToHere(segue: UIStoryboardSegue) {
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
         
     }
     
-    // This action allows the register view controller to pop back to here AFTER creating an account
-    @IBAction func exitToHereFromRegister(segue: UIStoryboardSegue) {
+    
+    
+    
+    // This action allows the register view controller to pop back to here
+    @IBAction func exitToHere(segue: UIStoryboardSegue) {
         
     }
 
@@ -36,15 +45,71 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func shouldPerformSegueWithIdentifier(identifier: String?, sender: AnyObject?) -> Bool
+    {
+        
+        if (identifier == "login") {
+            let query = PFQuery(className:"Operator")
+            query.whereKey("username", equalTo: username.text!)
+            print("up here")
+            do {
+                let objects = try query.findObjects()
+                if let objects = objects as [PFObject]! {
+                    for object in objects
+                    {
+                        print(self.password.text!)
+                        print((object["username"] as! String!))
+                        if (self.password.text! == (object["password"] as! String)) {
+                            print("match found")
+                            self.matchFound = true
+                            print(self.matchFound, " 1")
+                            break
+                        }
+                    }
+                }
+                else {
+                    print("didint work!")
+                }
+            }
+                
+            catch {
+                print("error occurred")
+            }
+            
+            print(self.matchFound, " 5")
+            if (self.matchFound) {
+                print("here")
+                self.matchFound = false
+                return true
+            }
+            else {
+                errMsg.text = "Incorrect username or password"
+                return false
+            }
+            
+        }
+        return true
+    }
+    
+    
+    
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "login")
+        {
+            let svc = segue.destinationViewController as! OwnerCartsTableViewController
+            svc.thisOwner = self.username.text!
+            
+        }
+        
     }
-    */
+    
 
 }
